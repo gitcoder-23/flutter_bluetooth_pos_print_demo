e2f5d2ca9b884da869d3a8f81e12066215293cf4
gitcoder-23 <prabhatece21@gmail.com>
2025-05-10 22:10:54 +0530

    need to test

--- lib/blue_printer_design.dart
+++ lib/blue_printer_design.dart
@@ -45,140 +45,135 @@ class _BluePrinterDesignState extends State<BluePrinterDesign> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: const Text('Bluetooth Printer'),
-        centerTitle: true,
-      ),
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.only(bottom: 20),
-        child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(15),
-                  backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () async {
-                  setState(() {
-                    selectedFile = null;
-                    selectedFilePath = null;
-                    isPDF = false;
-                  });
-                },
-                child: Text(
-                  'Cancel',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(15),
-                  backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () async {
-                  final address =
-                      await FlutterBluetoothPrinter.selectDevice(context);
-                  if (address != null) {
-                    await controller?.print(
-                      address: address.address,
-                      keepConnected: true,
-                      addFeeds: 4,
-                    );
-                  } else {
-                    log('Failed Printing!');
-                  }
-                },
-                child: Text(
-                  'Select Printer & Print',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: const Text('Bluetooth Printer'),
+          centerTitle: true,
         ),
-      ),
-      body: selectedFilePath == null
-          ? Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'No file selected',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.all(8.0),
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(15),
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: pickAndUploadFile, // Upload PDF button
-                    child: Text('Upload File',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        )),
+                  onPressed: () async {
+                    setState(() {
+                      selectedFile = null;
+                      selectedFilePath = null;
+                      isPDF = false;
+                    });
+                  },
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ],
-              ),
-            )
-          : Receipt(
-              builder: (context) => Container(
-                width: double.infinity,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(15),
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () async {
+                    final address =
+                        await FlutterBluetoothPrinter.selectDevice(context);
+                    if (address != null) {
+                      await controller?.print(
+                        address: address.address,
+                        keepConnected: true,
+                        addFeeds: 4,
+                      );
+                    } else {
+                      log('Failed Printing!');
+                    }
+                  },
+                  child: Text(
+                    'Select Printer & Print',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: selectedFilePath == null
+            ? Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'No file selected',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: pickAndUploadFile, // Upload PDF button
+                      child: Text('Upload File',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          )),
+                    ),
+                  ],
+                ),
+              )
+            : Receipt(
+                builder: (context) => Column(
                   children: [
                     if (selectedFilePath != null)
                       isPDF
-                          ? SingleChildScrollView(
-                              scrollDirection: Axis.vertical,
-                              child: SizedBox(
-                                height: 300,
-                                width: 200,
-                                child: PDFView(
-                                  filePath: selectedFilePath,
-                                ),
+                          ? SizedBox(
+                              height: 500,
+                              // width: double.infinity,
+                              child: PDFView(
+                                filePath: selectedFilePath,
                               ),
                             )
                           : SingleChildScrollView(
                               scrollDirection: Axis.vertical,
-                              child: Image.file(
-                                File(selectedFilePath!),
-                                height: 300,
-                                width: 200,
-                                fit: BoxFit.contain,
+                              child: SizedBox(
+                                width: double
+                                    .infinity, // Ensure it takes full width
+                                child: Image.file(
+                                  File(selectedFilePath!),
+                                  fit: BoxFit.contain,
+                                ),
                               ),
-                            )
+                            ),
                   ],
                 ),
-              ),
-              onInitialized: (controller) {
-                this.controller = controller;
-              },
-            ),
-    );
+                onInitialized: (controller) {
+                  this.controller = controller;
+                },
+              ));
   }
 }
